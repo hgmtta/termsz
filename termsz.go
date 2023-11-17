e8308e0a3d80a7e6c143d74edda40b9a293812e4
Hugo Marotta <hgmtta@proton.me>
2023-11-17 18:07:10 -0300

    feat: add `GetColumns` to get terminal window columns number
    This function returns the number of columns as an integer and an error
    if any occurs.

--- termsz.go
+++ termsz.go
@@ -5,3 +5,10 @@ package termsz
 func GetSize() (cols, rows int, err error) {
 	return getSize()
 }
+
+// GetColumns returns the number of columns in the current terminal window.
+// It returns the number of columns as an integer and an error if any occurs.
+func GetColumns() (int, error) {
+	cols, _, err := GetSize()
+	return cols, err
+}
